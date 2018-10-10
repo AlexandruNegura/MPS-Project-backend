edcda69073237709644026b716dc8a98f7731e59
AlexandruNegura <alexdn13@gmail.com>
2018-10-10 22:27:24 +0300

    Add skeleton for backend

---
+++ src/main/java/mps/fifa/FifaApplication.java
@@ -0,0 +1,37 @@
+package mps.fifa;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class FifaApplication {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifaApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+}
