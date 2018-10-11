6c7a991fb5052e3de99ce4b3ee1ea00cbfd5eddd
Alexdnn <alexandru.negura@ab4.systems>
2018-10-12 00:48:40 +0300

    Create baseline for project structure

--- src/main/java/mps/fifa/FifaApplication.java
+++ src/main/java/mps/fifa/FifaApplication.java
@@ -1,37 +1,15 @@
 package mps.fifa;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class FifaApplication {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Autowired
-    private DataSource dataSource;
+public class FifaApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(FifaApplication.class, args);
     }
 
-    @Bean
-    public DataSource dataSource() throws SQLException {
-        if (dbUrl == null || dbUrl.isEmpty()) {
-            return new HikariDataSource();
-        } else {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl(dbUrl);
-            return new HikariDataSource(config);
-        }
-    }
 }
