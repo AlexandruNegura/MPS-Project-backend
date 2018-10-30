f9485a4aaa4ec6bdd05baa3b67c5af91592b490a
alexandrumc <alexandru.cmilitaru@gmail.com>
2018-10-30 10:43:02 +0200

    Added Parser

---
+++ src/main/java/mps/fifa/ParseTest.java
@@ -0,0 +1,9 @@
+package mps.fifa;
+
+public class ParseTest {
+	public static void main(String[] args) {
+		Parser p = new Parser();
+		p.parse("resources/CSV_all.csv");
+		p.sendParsedDataToDatabase();
+	}
+}
